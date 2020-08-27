fd00bd2789d5085181a6e2a7c0c6bb4d17344957
oneten <onetenten6@gmail.com>
2020-08-27 07:15:13 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -37,7 +37,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -77,7 +82,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task=Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content=$request->content;
         $task->save();
         return redirect('/');
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
 <!-- ここにページ毎のコンテンツを書く -->
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
@@ -9,6 +8,11 @@
         <div class="col-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
